803dc54116c3d6c1705628b53bd1395c68dd4f02
Raul Ramirez <raul.ramiros07@gmail.com>
2026-06-13 14:58:55 -0600

    fix: register SocketBridgePlugin in MainActivity, remove stale capacitor.js tag, add bridge-ready wait

--- SmartHomePro-Android/android/app/src/main/java/com/smarthomepro/app/MainActivity.java
+++ SmartHomePro-Android/android/app/src/main/java/com/smarthomepro/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.smarthomepro.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(SocketBridgePlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
